a6b88e6b2dc98da18167133c97c4b9b9b4608c04
Cristobal <62496161+cristobalcuevas@users.noreply.github.com>
2024-07-22 19:14:57 +0000

    added html and css

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
-
-
-@app.route('/route')
-def route():
-    return 'Hello from new route!'
-
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
